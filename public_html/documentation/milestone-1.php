401aa8f9b974acd41dfa91beed1af0f9dab6b41c
Bernina Gray <bernina.gray@icloud.com>
2018-10-17 22:09:04 -0600

    Initial commit for pwp project. Godspeed.

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Bernina Gray Personal Website Project</title>
+	</head>
+	<body>
+		<h2>Bernina Gray</h2>
+		<h4>Purpose:</h4>
+		<p>To establish myself as an entry-level asset to a local company that is willing to
+			teach and mold me as an member of their team.</p>
+	</body>
+</html>
