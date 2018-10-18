cd012dcfbb68cd21e986b25621e266e2d8075984
Bernina Gray <bernina.gray@icloud.com>
2018-10-18 07:38:25 -0600

    Test Test

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -6,8 +6,27 @@
 	</head>
 	<body>
 		<h2>Bernina Gray</h2>
-		<h4>Purpose:</h4>
-		<p>To establish myself as an entry-level asset to a local company that is willing to
-			teach and mold me as an member of their team.</p>
+		<h3>Web Developer</h3>
+		<h3>Purpose, Audience & Goals</h3>
+			<h4>Purpose:</h4>
+				<p>To establish myself as an entry-level asset to a local company that is willing to
+				teach and mold me as an member of their team.</p>
+			<h4>Audience:</h4>
+				<p>This site was designed to attract potential employers, investors, direct clientele<b/>
+				and business partners alike. Target audience includes corporations and companies that <b/>
+				that are seeking recently trained, competent individuals that are teachable and able to <b/>
+				a business' culture and technological needs.</p>
+			<h4>Goal:</h4>
+				<p>The main goal of this site is to spur enough interest from potential employers and <b/>
+				provide access to my projects and share mny contact information.</p>
+		<h3>Persona</h3>
+			<h4>Name:</h4>
+				<p>Paul Wise</p>
+			<h4>Profession:</h4>
+				<p>Co-CEO of Web Development & Design Firm</p>
+			<h4>Technology:</h4>
+				<ul>
+					<li>Macbook Pro</li>
+				</ul>
 	</body>
 </html>
