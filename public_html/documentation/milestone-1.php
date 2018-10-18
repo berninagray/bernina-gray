d7bce37342c63d50528c407600d9add7655a0f19
Bernina Gray <bernina.gray@icloud.com>
2018-10-18 08:32:53 -0600

    commit again getting 502 bad error message

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -9,28 +9,28 @@
 		<h2>Bernina Gray</h2>
 		<h3>Web Developer</h3>
 		<h3>Purpose, Audience & Goals</h3>
-			<h4>Purpose:</h4>
+			<strong>Purpose:</strong>
 				<p>To establish myself as an entry-level asset to a local company that is willing to
 				teach and mold me as an member of their team.</p>
-			<h4>Audience:</h4>
+			<strong>Audience:</strong>
 				<p>This site was designed to attract potential employers, investors, direct clientele<b/>
 				and business partners alike. Target audience includes corporations and companies that <b/>
 				that are seeking recently trained, competent individuals that are teachable and able to <b/>
 				a business' culture and technological needs.</p>
-			<h4>Goal:</h4>
+			<strong>Goal:</strong>
 				<p>The main goal of this site is to spur enough interest from potential employers and <b/>
 				provide access to my projects and share mny contact information.</p>
 		<h3>Persona</h3>
-			<h4>Name:</h4>
+			<strong>Name:</strong>
 				<p>Paul Wise</p>
-			<h4>Profession:</h4>
+			<strong>Profession:</strong>
 				<p>Co-CEO of Web Development & Design Firm</p>
-			<h4>Technology:</h4>
+			<strong>Technology:</strong>
 				<ul>
 					<li>Macbook Pro - 15" model with Touch Bar, 4.5 GHz Turbo Boost, 32 GB of DDR4 memory</li>
 					<li>iphone X - </li>
 				<ul>
-			<h4>Attitudes & Behavior</h4>
+			<strong>Attitudes & Behavior</strong>
 				<p>Self-taught programmer from the age of 13, specializing in Javascript and C#, attended <b/>
 					attended MIT on a full scholarship, studied Computer Science. Partnered with his <b/>
 					significant other to start their own Web Development and Design Firm.</p>
