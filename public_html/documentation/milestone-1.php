a017d01208e90fa81e11ad08b3b0386ad5d95461
Bernina Gray <bernina.gray@icloud.com>
2018-10-18 10:25:04 -0600

    Commit after moving directories.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -6,50 +6,52 @@
 		<title>Bernina Gray Personal Website Project</title>
 	</head>
 	<body>
+		<div><
 		<h2>Bernina Gray</h2>
 		<h3>Web Developer</h3>
 		<h3>Purpose, Audience & Goals</h3>
-			<strong>Purpose:</strong>
+			<p><strong>Purpose:</strong></p>
 				<p>To establish myself as an entry-level asset to a local company that is willing to
 				teach and mold me as an member of their team.</p>
-			<strong>Audience:</strong>
+			<p><strong>Audience:</strong></p>
 				<p>This site was designed to attract potential employers, investors, direct clientele<b/>
 				and business partners alike. Target audience includes corporations and companies that <b/>
 				that are seeking recently trained, competent individuals that are teachable and able to <b/>
 				a business' culture and technological needs.</p>
-			<strong>Goal:</strong>
+			<p><strong>Goal:</strong></p>
 				<p>The main goal of this site is to spur enough interest from potential employers and <b/>
 				provide access to my projects and share mny contact information.</p>
+		</div>
 		<h3>Persona</h3>
-			<strong>Name:</strong>
+			<p><strong>Name:</strong></p>
 				<p>Paul Wise</p>
-			<strong>Profession:</strong>
+			<p><strong>Profession:</strong></p>
 				<p>Co-CEO of Web Development & Data Design Firm</p>
-			<strong>Technology:</strong>
+			<p><strong>Technology:</strong></p>
 				<ul>
 					<li>OS - MacOs & iOs</li>
 					<li>Macbook Pro - 15" model with Touch Bar, 4.5 GHz Turbo Boost, 32 GB of DDR4 memory</li>
 					<li>iphone XS for business - 512GB Storage, Apple A12 Bionic, 6-core processor, and <b/>
 					Super Retina OLED Display and 4K HD video.</li>
 				<ul>
-			<strong>Attitudes & Behavior:</strong>
+			<p><strong>Attitudes & Behavior:</strong></p>
 				<p>Self-taught programmer from the age of 13, specializing in Javascript and C#, attended <b/>
 					attended MIT on a full scholarship, studied Computer Science. Partnered with his <b/>
 					significant other to start their own Web Development and Design Firm. List of cientele</b>
 					and size of projects are growing so more team members are currently being sought. </p>
-			<strong>Frustration & Needs:</strong>
+			<p><strong>Frustration & Needs:</strong></p>
 					<p>Paul is looking to hire a team of developers that are able to collaborate remotely<b/>
 					He is seeking talented individuals that are teachable and willing to learn constantly.<b/>
 					His frustrations include working in a structured corporation, thus this entrepreneurship<b/>
 					and slow internet connection from partially residing in mountainous areas in the summertime<b/>
 					He requires employees with access to high-speed internet and is currently undergoing <b/>
 					cloud migration. He is looking to hire immediately.</p>
-			<strong>Goals:</strong>
+			<p><strong>Goals:</strong></p>
 					<p>Paul's main goal is to acquire and form a reliable and competent team within two weeks.<b/>
 					He is only speaking to the web developers that are able to pass his competency test within<b/>
 					amount of time specified. He is also looking at potential employees creativity and ability<b/>
 						integrate new ideas into their work to enhance productivity and standards.</p>
-			<strong>User:</strong>
+			<p><strong>User:</strong></p>
 					<p>As a user, Paul needs to quickly and efficiently determine a potential candidate's<b/>
 					profile after inspecting the elements on their websites to find a good fit for a person <b/>
 					skilled in both front and back-end web developement, as well as Data Design.<p/>
