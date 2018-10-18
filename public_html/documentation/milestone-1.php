fd9aa2f20ddfbcfc9b507eb1842081f7f883a2eb
Bernina Gray <bernina.gray@icloud.com>
2018-10-18 10:45:20 -0600

    COMMIT

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -19,16 +19,16 @@
 			<p><strong>Audience:</strong></p>
 		</div>
 		<div>
-				<p>This site was designed to attract potential employers, investors, direct clientele<b>
-				and business partners alike. Target audience includes corporations and companies that <b>
-				that are seeking recently trained, competent individuals that are teachable and able to <b>
+				<p>This site was designed to attract potential employers, investors, direct clientele
+				and business partners alike. Target audience includes corporations and companies that
+				that are seeking recently trained, competent individuals that are teachable and able to
 				a business' culture and technological needs.</p>
 		</div>
 		<div>
 			<p><strong>Goal:</strong></p>
 		</div>
 		<div>
-			<p>The main goal of this site is to spur enough interest from potential employers and <b/>
+			<p>The main goal of this site is to spur enough interest from potential employers and
 				provide access to my projects and share mny contact information.</p>
 		</div>
 		</div>
@@ -44,31 +44,31 @@
 				<ul>
 					<li>OS - MacOs & iOs</li>
 					<li>Macbook Pro - 15" model with Touch Bar, 4.5 GHz Turbo Boost, 32 GB of DDR4 memory</li>
-					<li>iphone XS for business - 512GB Storage, Apple A12 Bionic, 6-core processor, and <b/>
+					<li>iphone XS for business - 512GB Storage, Apple A12 Bionic, 6-core processor, and
 					Super Retina OLED Display and 4K HD video.</li>
 				<ul>
 		</div>
 		<div>
 			<p><strong>Attitudes & Behavior:</strong></p>
-				<p>Self-taught programmer from the age of 13, specializing in Javascript and C#, attended <b/>
-					attended MIT on a full scholarship, studied Computer Science. Partnered with his <b/>
-					significant other to start their own Web Development and Design Firm. List of cientele</b>
+				<p>Self-taught programmer from the age of 13, specializing in Javascript and C#, attended
+					attended MIT on a full scholarship, studied Computer Science. Partnered with his
+					significant other to start their own Web Development and Design Firm. List of cientele
 					and size of projects are growing so more team members are currently being sought. </p>
 			<p><strong>Frustration & Needs:</strong></p>
-					<p>Paul is looking to hire a team of developers that are able to collaborate remotely<b/>
-					He is seeking talented individuals that are teachable and willing to learn constantly.<b/>
-					His frustrations include working in a structured corporation, thus this entrepreneurship<b/>
-					and slow internet connection from partially residing in mountainous areas in the summertime<b/>
-					He requires employees with access to high-speed internet and is currently undergoing <b/>
+					<p>Paul is looking to hire a team of developers that are able to collaborate remotely
+					He is seeking talented individuals that are teachable and willing to learn constantly.
+					His frustrations include working in a structured corporation, thus this entrepreneurship
+					and slow internet connection from partially residing in mountainous areas in the summertime
+					He requires employees with access to high-speed internet and is currently undergoing
 					cloud migration. He is looking to hire immediately.</p>
 			<p><strong>Goals:</strong></p>
-					<p>Paul's main goal is to acquire and form a reliable and competent team within two weeks.<b/>
-					He is only speaking to the web developers that are able to pass his competency test within<b/>
-					amount of time specified. He is also looking at potential employees creativity and ability<b/>
+					<p>Paul's main goal is to acquire and form a reliable and competent team within two weeks.
+					He is only speaking to the web developers that are able to pass his competency test within
+					amount of time specified. He is also looking at potential employees creativity and ability
 						integrate new ideas into their work to enhance productivity and standards.</p>
 			<p><strong>User:</strong></p>
-					<p>As a user, Paul needs to quickly and efficiently determine a potential candidate's<b/>
-					profile after inspecting the elements on their websites to find a good fit for a person <b/>
+					<p>As a user, Paul needs to quickly and efficiently determine a potential candidate's
+					profile after inspecting the elements on their websites to find a good fit for a person
 					skilled in both front and back-end web developement, as well as Data Design.<p/>
 		</div>
 	</body>
