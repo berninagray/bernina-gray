2b98d65d295d7d6fc2291b8d9c2a78dc82797d19
Bernina Gray <bernina.gray@icloud.com>
2018-10-18 09:20:11 -0600

    Attempt to commit and push.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -24,20 +24,35 @@
 			<strong>Name:</strong>
 				<p>Paul Wise</p>
 			<strong>Profession:</strong>
-				<p>Co-CEO of Web Development & Design Firm</p>
+				<p>Co-CEO of Web Development & Data Design Firm</p>
 			<strong>Technology:</strong>
 				<ul>
+					<li>OS - MacOs & iOs</li>
 					<li>Macbook Pro - 15" model with Touch Bar, 4.5 GHz Turbo Boost, 32 GB of DDR4 memory</li>
-					<li>iphone X - </li>
+					<li>iphone XS for business - 512GB Storage, Apple A12 Bionic, 6-core processor, and <b/>
+					Super Retina OLED Display and 4K HD video.</li>
 				<ul>
-			<strong>Attitudes & Behavior</strong>
+			<strong>Attitudes & Behavior:</strong>
 				<p>Self-taught programmer from the age of 13, specializing in Javascript and C#, attended <b/>
 					attended MIT on a full scholarship, studied Computer Science. Partnered with his <b/>
 					significant other to start their own Web Development and Design Firm. List of cientele</b>
 					and size of projects are growing so more team members are currently being sought. </p>
-			<strong>Frustration & Needs</strong>
+			<strong>Frustration & Needs:</strong>
 					<p>Paul is looking to hire a team of developers that are able to collaborate remotely<b/>
-					</p>
+					He is seeking talented individuals that are teachable and willing to learn constantly.<b/>
+					His frustrations include working in a structured corporation, thus this entrepreneurship<b/>
+					and slow internet connection from partially residing in mountainous areas in the summertime<b/>
+					He requires employees with access to high-speed internet and is currently undergoing <b/>
+					cloud migration. He is looking to hire immediately.</p>
+			<strong>Goals:</strong>
+					<p>Paul's main goal is to acquire and form a reliable and competent team within two weeks.<b/>
+					He is only speaking to the web developers that are able to pass his competency test within<b/>
+					amount of time specified. He is also looking at potential employees creativity and ability<b/>
+						integrate new ideas into their work to enhance productivity and standards.</p>
+			<strong>User:</strong>
+					<p>As a user, Paul needs to quickly and efficiently determine a potential candidate's<b/>
+					profile after inspecting the elements on their websites to find a good fit for a person <b/>
+					skilled in both front and back-end web developement, as well as Data Design.<p/>
 </body>
 	
 </html>
