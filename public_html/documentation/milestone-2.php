4729561b0a5815feeed26614344ef3c55851e065
Bernina Gray <bernina.gray@icloud.com>
2018-10-22 08:00:46 -0600

    Adding Milestone 2

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -7,7 +7,7 @@
 	<body>
 		<h2>Content Strategy</h2>
 		<p>My site will be one page with professional links and will be one page consisting of the following sections:</p>
-		<strong>Welcome:</strong>
+		<p><strong>Welcome:</strong></p>
 		<p>Here will be a brief single line introduction that states my name and role as a Fullstack Web Developer. </p>
 		<p><strong>About Me:</strong></p>
 		<p>This section will highlight my professional history, with emphasis on my recently acquired technical skills. <br>
@@ -15,7 +15,16 @@
 		or major block background color on my homepage. My "Welcome" section will be found near the center of the homepage. <br>
 		My "About Me" will be featured with a left page transition and my "Portfolio" section will be featured <br>
 		with a homepage down transition. Lastly, my "Contact" info will be found in a left page transition. </p>
-		<p><strong></strong></p>
-		<h3></h3>
+		<p><strong>Porfolio:</strong></p>
+		<p>I want this section to be somewhat interactive to keep interest, but with minimal instruction. My projects will be <br>
+		navigatable with a sort of rotating image carousel effect, with each project in its' own space. It will feature 4 <br>
+		examples of my best work.</p>
+		<p><strong>Contact:</strong></p>
+		<p>The contact section will include my professional email link. There will also be links to Github and LinkedIn.<br>
+		</p>
+		<h2>Wireframes</h2>
+		<p>This site will feature one layout on both mobile and desktop screens. These wireframes were created using <br>
+		<a href="https://mockflow.com">Mockflow.</a></p>
+
 	</body>
 </html>
