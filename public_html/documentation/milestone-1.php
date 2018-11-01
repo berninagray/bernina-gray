98109cf9fa3250313102fea80e8783170d8b612f
Bernina Gray <bernina.gray@icloud.com>
2018-11-01 09:09:47 -0600

    committing before sending link

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -81,14 +81,15 @@
 			<p>This section will highlight my professional history, with emphasis on my recently acquired technical skills. <br>
 				My strengths, passion and a bit of creativity will be implemented as well. I will not have much color <br>
 				or major block background color on my homepage. My "Welcome" section will be found near the center of the homepage. <br>
-				My "About Me" will be featured with a left page transition and my "Portfolio" section will be featured <br>
-				with a homepage down transition. Lastly, my "Contact" info will be found in a left page transition. </p>
+				My "About Me" will be featured after my projects and my "Portfolio" section will be featured <br>
+				with a homepage down transition. Lastly, my "Contact" info will be found at the very bottom of the page. <br>
+				</p>
 			<p><strong>Porfolio:</strong></p>
-			<p>I want this section to be somewhat interactive to keep interest, but with minimal instruction. My projects will be <br>
-				navigatable with a sort of rotating image carousel effect, with each project in its' own space. It will feature 4 <br>
+			<p>I want this section to be clean, simpleeand able to keep a visitor's interest. My projects will be <br>
+				navigatable with a swipe down action with each project in its' own space. It will feature 4 <br>
 				examples of my best work.</p>
 			<p><strong>Contact:</strong></p>
-			<p>The contact section will include my professional email link. There will also be links to Github and LinkedIn.<br>
+			<p>The contact section will include my professional email link. There will also be link icons to Github and LinkedIn.<br>
 			</p>
 			<h2>Wireframes</h2>
 			<p>This site will feature one layout on both mobile and desktop screens. These wireframes were created using <br>
