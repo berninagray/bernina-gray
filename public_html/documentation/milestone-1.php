6104e225a6808914ad169cc089bfc76855ed9690
Bernina Gray <bernina.gray@icloud.com>
2018-11-01 09:47:12 -0600

    committing before sending link

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -72,33 +72,6 @@
 					<p>As a user, Paul needs to quickly and efficiently determine a potential candidate's
 					profile after inspecting the elements on their websites to find a good fit for a person <br>
 					skilled in both front and back-end web development, as well as Data Design.</p>
-		<div>
-			<h2>Content Strategy</h2>
-			<p>My site will be one page with professional links and will be one page consisting of the following sections:</p>
-			<p><strong>Welcome:</strong></p>
-			<p>Here will be a brief single line introduction that states my name and role as a Fullstack Web Developer. </p>
-			<p><strong>About Me:</strong></p>
-			<p>This section will highlight my professional history, with emphasis on my recently acquired technical skills. <br>
-				My strengths, passion and a bit of creativity will be implemented as well. I will not have much color <br>
-				or major block background color on my homepage. My "Welcome" section will be found near the center of the homepage. <br>
-				My "About Me" will be featured after my projects and my "Portfolio" section will be featured <br>
-				with a homepage down transition. Lastly, my "Contact" info will be found at the very bottom of the page. <br>
-				</p>
-			<p><strong>Porfolio:</strong></p>
-			<p>I want this section to be clean, simpleeand able to keep a visitor's interest. My projects will be <br>
-				navigatable with a swipe down action with each project in its' own space. It will feature 4 <br>
-				examples of my best work.</p>
-			<p><strong>Contact:</strong></p>
-			<p>The contact section will include my professional email link. There will also be link icons to Github and LinkedIn.<br>
-			</p>
-			<h2>Wireframes</h2>
-			<p>This site will feature one layout on both mobile and desktop screens. These wireframes were created using <br>
-				<a href="https://mockflow.com">Mockflow.</a></p>
-			<div>
-				<p><strong>Mobile Wireframe</strong></p>
-				<img src="wireframeI.png" alt="Mobile Wireframe">
-				<p><strong>Desktop Wireframe</strong></p>
-				<img src="wireframeII.png" alt="Mobile Wireframe">
 		</div>
 	</body>
 </html>
