483953848265c3e65c88c0fc04f99adb7139adec
Bernina Gray <bernina.gray@icloud.com>
2018-11-01 13:19:33 -0600

    Corrected misspelling and links.

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -27,9 +27,8 @@
 			<h2>Wireframes</h2>
 			<p>This site will feature one layout on both mobile and desktop screens. These wireframes were created using <br>
 				<a href="https://mockflow.com">Mockflow.</a></p>
-			<div>
 				<p><strong>Mobile Wireframe</strong></p>
-				<img src="wireframeI.png" alt="Mobile Wireframe">
+				<img src="milestone-2-mobile-wireframe.html" alt="Mobile Wireframe">
 				<p><strong>Desktop Wireframe</strong></p>
 				<img src="wireframeII.png" alt="Mobile Wireframe">
 		</div>
