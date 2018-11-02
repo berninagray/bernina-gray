9ec1d0316e8ee13385009e06dedad34942541717
Bernina Gray <bernina.gray@icloud.com>
2018-11-02 08:20:55 -0600

    Photos still not working.

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -28,11 +28,9 @@
 			<p>This site will feature one layout on both mobile and desktop screens. These wireframes were created using <br>
 				<a href="https://mockflow.com">Mockflow.</a></p>
 				<p><strong>Mobile Wireframe</strong></p>
-				<img src="mobile-wireframe.png" alt="Mobile Wireframe"
-				height="250">
+				<img src="mobile-wireframe.png" alt="Mobile Wireframe">
 				<p><strong>Desktop Wireframe</strong></p>
-				<img src="desktop-wireframe.png" alt="Mobile Wireframe"
-				height="250">
+				<img src="desktop-wireframe.png" alt="Mobile Wireframe">
 		</div>
 	</body>
 </html>
