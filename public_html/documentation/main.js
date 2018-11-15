49710962484b2965cee8aa22801d5a137bbacd3c
Bernina Gray <bernina.gray@icloud.com>
2018-11-15 13:58:47 -0700

    attempt to add background image to header

--- public_html/documentation/main.js
+++ public_html/documentation/main.js
@@ -1,8 +1,6 @@
 <script type="text/" src='js/main.js'></script>
-	/*header style*/
-	.header{
-	background-image: url('../images/headerback.jpg');
-	background-attachment: fixed;
-	background-size: cover;
-	background-position: center;
-}
+
+$(document).ready(function(){
+	$('.header').height($(window).height());
+
+})
