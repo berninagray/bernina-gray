f85b7b32f923e7823bd3f5849b0364475d2da34a
Bernina Gray <bernina.gray@icloud.com>
2018-11-15 13:37:30 -0700

    fixed navbar comment

--- public_html/documentation/berninagray.js
+++ public_html/documentation/berninagray.js
@@ -1,1 +1,8 @@
 <script type="text/" src='js/main.js'></script>
+	/*header style*/
+	.header{
+	background-image: url('../images/headerback.jpg');
+	background-attachment: fixed;
+	background-size: cover;
+	background-position: center;
+}
