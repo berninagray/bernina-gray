0c10d08005515433825f46a3a73379564369f3be
Bernina Gray <bernina.gray@icloud.com>
2018-11-19 10:49:24 -0700

    Adding rules for contact form

--- public_html/documentation/main.js
+++ public_html/documentation/main.js
@@ -1,4 +1,26 @@
 $(document).ready(function(){
-	$('.header').height($(window).height());
+	$("form").validate({
+		// setup handling of form errors
+		debug: true,
+		errorClass: "alert alert-danger",
+		errorLabelContainer: "#output-area",
+		errorElement: "div",
+
+		// rules here define what is good or bad input
+		// each rule starts with the form input element's name attribute
+		rules: {
+			name: {
+				required: true
+			},
+			email: {
+				email: true,
+				required: true
+			},
+			message: {
+				required: true,
+				maxlength: 2000
+			}
+		},
+	})
 
 })
