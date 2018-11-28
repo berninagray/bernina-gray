237ae2a51905592c84309c0b8a17aa2173601cbe
Bernina Gray <bernina.gray@icloud.com>
2018-11-28 16:03:08 -0700

    added recaptcha to index.php

--- public_html/index.php
+++ public_html/index.php
@@ -150,7 +150,7 @@
 			</div>
 
 			<!-- reCAPTCHA -->
-			<div class="g-recaptcha" data-sitekey="--YOUR RECAPTCHA SITE KEY--"></div>
+			<div class="g-recaptcha" data-sitekey="6Ld3zHsUAAAAAMCnTe5bTf0nIl9ho-hmKE6meKi1"></div>
 
 			<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send</button>
 			<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
